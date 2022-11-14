405dc08dde4541003dd970e0af2876523a3230dc
Kenni Woodard <kennikristine@gmail.com>
2022-11-14 14:42:00 -0800

    clicking a dog takes user to detail page

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,18 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+    // console.log(window.location.search, '.search');
+    const id = params.get('id');
+
+    console.log(id);
+
+    const dog = await getDog(id);
+
+    const dogDetailEl = renderDogDetail(dog);
+    dogDetailContainer.append(dogDetailEl);
+});
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
